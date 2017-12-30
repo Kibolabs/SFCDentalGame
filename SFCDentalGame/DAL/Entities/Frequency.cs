dc22aa9b72c09c1135f8a97e831ae398f508e973
KIBO <kibo@KIBOs-MacBook-Pro.local>
2017-12-29 20:48:51 -0500

    Updated data models, Implementation of health pages

---
+++ SFCDentalGame/DAL/Entities/Frequency.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace SFCDentalGame.DAL.Entities
+{
+    public class BehaviourFrequency:Behaviour
+    {
+        public BehaviourFrequency()
+        {
+        }
+        public int FrequencyId
+        {
+            get;
+            set;
+        }
+        public string FrequencyName
+        {
+            get;
+            set;
+        }
+        public decimal FrequencyPoints
+        {
+            get;
+            set;
+        }
+    }
+}
